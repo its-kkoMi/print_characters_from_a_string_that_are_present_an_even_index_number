9de73c9922f9f187a8327644c685cc85e52e8151
James Mikko P. Recario <jamesmikkoprecario@iskolarngbayan.pup.edu.ph>
2024-01-25 23:59:11 +0800

    Program Instructions and Pseudocode

---
+++ print_characters_from_a_string_that_are_present_an_even_index_number.py
@@ -0,0 +1,8 @@
+# Assignment 5 - Exercise 3
+# Write a program to accept a string from the user and display characters that are present at an even index number.
+# For example, str = "pynative" so you should display ‘p’, ‘n’, ‘t’, ‘v’.
+
+# Ask user for input
+# Convert string to list
+# Use list slicing
+# Print even index characters
